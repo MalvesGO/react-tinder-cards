f942dcfd451729c2108ecebd1d7add6909edf8df
Marcelo Alves <marceloalves_ac@hotmail.com>
2019-09-04 16:59:02 -0300

    teste

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 export default [
   {
     pics: [
-      "https://images.unsplash.com/photo-1520024146169-3240400354ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+      "https://scontent.fgyn2-1.fna.fbcdn.net/v/t1.0-9/26993704_1629355217159380_3137881641012763428_n.jpg?_nc_cat=111&_nc_oc=AQmyssDH2qaDsjNRuQzUdhcugWMVh8x5uWEssfPxyTrqk0h4mBChSA23Wq51FPDh-f0&_nc_ht=scontent.fgyn2-1.fna&oh=4c4effea4a4c450fb90d93cc5346a636&oe=5E0B1FE9"
     ],
-    name: "Heather",
+    name: "Marina",
     age: 32,
     distance: "4 miles away",
     text:
@@ -11,9 +11,9 @@ export default [
   },
   {
     pics: [
-      "https://images.unsplash.com/photo-1522263842439-347f062b8475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+      "https://scontent.fgyn2-1.fna.fbcdn.net/v/t1.0-9/37533588_2056233641078053_1169610755381657600_n.jpg?_nc_cat=100&_nc_oc=AQm8Y3HuWM5tHQiUP0BiqF0x0FJAklhviAm2x4ClOnfCOVoAga-blAxDUD2IjMcl6nI&_nc_ht=scontent.fgyn2-1.fna&oh=edbd96e9b46c2ae564e45a91cf849870&oe=5E034BA5"
     ],
-    name: "Chloe",
+    name: "Simone",
     age: 18,
     distance: "1 mile away",
     text: "The C and the L are silent."
@@ -22,7 +22,7 @@ export default [
     pics: [
       "https://images.unsplash.com/photo-1539758462369-43adaa19bc1f?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
     ],
-    name: "Helen",
+    name: "Heloísa",
     age: 19,
     distance: "3 miles away",
     text: "Aspiring MILF"
@@ -31,7 +31,7 @@ export default [
     pics: [
       "https://images.unsplash.com/photo-1514924801778-1db0aba75e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
     ],
-    name: "Savannah",
+    name: "Priscila",
     age: 29,
     distance: "3 miles away",
     text: "A little known fact is that I cover about 40% of Africa."
